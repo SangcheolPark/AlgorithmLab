54d6c45b0032134829de16cd98011b2b8482ccf1
Sangchol.Park <sangchol.park@outlook.com>
2016-02-14 01:22:11 +0900

    20160214_Add Message.

--- AlgorithmLab/src/HelloWorld.java
+++ AlgorithmLab/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello PKNU's AlgorithmLab.");
+		System.out.println("GitHub. It is very Simple.");
 
 	}
 
